859b24bf045ef33b5b62b53032856b4c6432a60d
Bommuraja <rbommuraja@outlook.com>
2019-02-25 10:42:10 +0530

    operator pages added

---
+++ src/app/pages/operators/operator-delete/operator-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operator-delete',
+  templateUrl: './operator-delete.component.html',
+  styleUrls: ['./operator-delete.component.scss']
+})
+export class OperatorDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,9 +8,14 @@ import { PagesComponent } from './pages.component';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { OperatorAddComponent } from './operators/operator-add/operator-add.component';
+import { OperatorEditComponent } from './operators/operator-edit/operator-edit.component';
+import { OperatorDeleteComponent } from './operators/operator-delete/operator-delete.component';
+import { OperatorViewComponent } from './operators/operator-view/operator-view.component';
+import { OperatorListComponent } from './operators/operator-list/operator-list.component';
 
 @NgModule({
-  declarations: [PagesComponent, SidebarComponent, HeaderComponent],
+  declarations: [PagesComponent, SidebarComponent, HeaderComponent, OperatorAddComponent, OperatorEditComponent, OperatorDeleteComponent, OperatorViewComponent, OperatorListComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
